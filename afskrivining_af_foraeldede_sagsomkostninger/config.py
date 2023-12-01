de1797a60597c4d8a961da82319edd84035dae22
Anders Krogsager <andkr@aarhus.dk>
2023-12-01 13:33:10 +0100

    queue name

--- afskrivining_af_foraeldede_sagsomkostninger/config.py
+++ afskrivining_af_foraeldede_sagsomkostninger/config.py
@@ -4,7 +4,7 @@ SMTP_PORT = 25
 SCREENSHOT_SENDER = "robot@friend.dk"
 MAX_RETRY_COUNT = 3
 #Configuration for the process, where runtime parameters are defined.
-QUEUE_NAME = 'fosa'
+QUEUE_NAME = 'Afskrivning-af-foraeldede-sagsomkostninger'
 SAP_CREDENTIALS = "Anders SAP"
 ERROR_EMAIL = "Error Email"
 DISABLE_DRY_RUN = "disable dry run"
